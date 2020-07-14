e58a52d76a78b04d2f339a48aeef5eaac6645e57
JawwadRaf98 <jawwad.rafique009@gmail.com>
2020-07-14 22:13:37 +0500

    Class 2 Code

---
+++ class1st.py
@@ -0,0 +1,75 @@
+# name = "jawwad"
+# print(name2, end=" ")
+# name2 = "Rafique Ahmed"
+# print(name2)
+# print("Bye")
+# # print()
+
+
+
+# name = "Jawwad"
+# weight = "12"
+# print(type(weight))
+# original_num = 23
+# num_to_be_added = 7
+# new_num = original_num + num_to_be_added
+# print(new_num)
+
+# original_num = 23
+# print(original_num)
+# original_num =  original_num + 7
+# print(original_num)
+
+# your_age = "21"
+# newage = your_age + " 7"
+# print(newage)
+
+# Math expression
+# a = 2.0
+# b = 4
+# c = 10
+# d = a+ b
+# / for float devision it return float data type
+# // for integer devision it return integer data type
+# when we apply any maths operators between float and int data type it must returns float data type
+# print(d)
+
+# a = 10 
+# b = 5
+# print(12%3)
+
+# even & odd number
+# a = 1009
+# if a % 2 == 0:
+#     print(a , "is even") 
+# else:
+#     print(a , "is odd") 
+
+# for leap year
+# year = 2016
+# if year % 4 == 0:
+#     print(year , "is leap year") 
+# else:
+#     print(year , "is not leap year") 
+
+# age = 2
+# # age += 7 means age =  age + 7
+# age **= 3
+# print(age)
+
+# Operator precedence
+# a  = 10
+# b = 12
+# c = 14 
+
+# d = a**2 * (b - c) 
+
+# print(d)
+
+# increment & decrement operator
+# a = 10
+# a = a + 1
+# a += 1
+# print(a )
+
+
---
+++ class2nd.py
@@ -0,0 +1,89 @@
+# concatination
+
+# name  = "Jawwad"
+# fName = "Rafique"
+# fullName = name + " "+ fName
+# print(fullName)
+
+# Azan Break 
+
+# print("Hello, " + "World!")
+# print("The sum of 2 plus 2 is " + "4")
+
+# IF Statement
+
+# num1 = 12
+# num2  = 14
+# if num1 > num2:
+#     print(num1, " is greater than ", num2 )
+#     print("if Condition is running")
+   
+# elif num1 == num2:
+#     print(num1, " is equals to ", num2 )
+#     print("elif Condition is running")
+    
+# else:
+#     print(num2, " is greater than ", num1 )
+#     print("else Condition is running")
+
+# name = "Hareem"
+
+# if name  == "Jawwad":
+#     print("Sir",name)
+# elif name == "Hafsa":
+#     print("rotoo", name)
+# elif name == "yusra":
+#     print("Hello "+ name)
+# else:
+#     print("Hello "+ name)
+#     print("else condition running")
+
+# ticketNo = 143
+# if ticketNo != 10:
+#     print("please be waited for your turn")
+# elif ticketNo == 10:
+#     print("Please come")
+
+# species = 'dog'
+# if species == "cat":
+#     print("Yep, it's cat.")
+
+
+
+# if species != "cat":
+#     print("Nope, not cat.")
+
+# Assignment no 1
+
+English = 
+Urdu =
+Maths = 
+Science = 
+Programming =
+
+obtMarks =
+totalMarks = 
+
+percentage = 
+
+grade = ""
+
+# if percentage >= 80 grade will be A+
+# if percentage >= 70 grade will be A
+# if percentage >= 60 grade will be B
+# if percentage >= 50 grade will be C
+# if percentage < 50 grade will be Fail
+
+# Then print complete markSheet as
+# SubjectName = obtMark out of totalMarks 
+# SubjectName = obtMark out of totalMarks 
+# SubjectName = obtMark out of totalMarks 
+# SubjectName = obtMark out of totalMarks 
+# SubjectName = obtMark out of totalMarks 
+# TotalMarks = 
+# obtained Mark = 
+# percentage =
+# Grade = 
+
+
+
